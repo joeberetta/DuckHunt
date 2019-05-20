d2ba12dce20b9159512b87a30f7381dc08305d73
Joe Beretta <shakhrukh.97@gmail.com>
2019-05-20 13:45:49 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,39 +1,40 @@
 function preloadImages(array, waitForOtherResources, timeout) {
-	var loaded = false, list = preloadImages.list, imgs = array.slice(0), t = timeout || 15 * 1000, timer;
-	if (!preloadImages.list) {
-		preloadImages.list = [];
-	}
-	if (!waitForOtherResources || document.readyState === 'complete') {
-		loadNow();
-	} else {
-		window.addEventListener("load", function () {
-			clearTimeout(timer);
-			loadNow();
-		});
-		// in case window.addEventListener doesn't get called (sometimes some resource gets stuck)
-		// then preload the images anyway after some timeout time
-		timer = setTimeout(loadNow, t);
-	}
+    var loaded = false, list = preloadImages.list, imgs = array.slice(0), t = timeout || 15*1000, timer;
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    if (!waitForOtherResources || document.readyState === 'complete') {
+        loadNow();
+    } else {
+        window.addEventListener("load", function() {
+            clearTimeout(timer);
+            loadNow();
+        });
+        // in case window.addEventListener doesn't get called (sometimes some resource gets stuck)
+        // then preload the images anyway after some timeout time
+        timer = setTimeout(loadNow, t);
+    }
 
-	function loadNow() {
-		if (!loaded) {
-			loaded = true;
-			for (var i = 0; i < imgs.length; i++) {
-				var img = new Image();
-				img.onload = img.onerror = img.onabort = function () {
-					var index = list.indexOf(this);
-					if (index !== -1) {
-						// remove image from the array once it loaded
-						// for memory consumption reasons
-						list.splice(index, 1);
-					}
-				}
-				list.push(img);
-				img.src = imgs[i];
-			}
-		}
-	}
+    function loadNow() {
+        if (!loaded) {
+            loaded = true;
+            for (var i = 0; i < imgs.length; i++) {
+                var img = new Image();
+                img.onload = img.onerror = img.onabort = function() {
+                    var index = list.indexOf(this);
+                    if (index !== -1) {
+                        // remove image from the array once it loaded
+                        // for memory consumption reasons
+                        list.splice(index, 1);
+                    }
+                }
+                list.push(img);
+                img.src = imgs[i];
+            }
+        }
+    }
 }
+
 preloadImages(["img/bg.jpg", "img/life++.svg", "img/life--.svg", "style/duckhunt.woff"], true);
 
 class Duck {
