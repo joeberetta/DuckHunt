b4bfe59dc56d2dfce23c39c7271301e4d6776fc5
Joe Beretta <shakhrukh.97@gmail.com>
2019-05-20 08:42:06 +0700

    Backup v1.0

---
+++ js/app.js
@@ -0,0 +1,157 @@
+class Duck {
+	constructor(width = 60, height = 75) {
+		this.width = (window.innerWidth < 1024) ? width : 1.5 * width,
+			this.height = (window.innerWidth < 1024) ? height : 1.5 * height,
+		this.top = Math.floor(Math.random() * screen.availHeight * 0.6 + screen.availHeight * 0.1),
+		this.direction = Math.floor(Math.random() * 1 - 0.5),
+		this.left = (this.direction < 0) ? (this.left = window.innerWidth + 16) : (this.left = 0 - (this.width + 16)),
+		this.speed = (Math.random() * 1 + 0.5),
+		this.FlyAnimation,
+		this.died = false,
+		this.Fly();
+	}
+	initDuck() {
+		let DuckHeart = document.createElement('input');
+		DuckHeart.type = 'checkbox';
+		DuckHeart.name = 'duck';
+
+		let Duck = document.createElement('label');
+		Duck.classList.add('duck');
+		Duck.appendChild(DuckHeart);
+		DuckHeart.addEventListener('click', () => {
+			Duck.remove();
+			this.Died();
+		})
+		if(this.direction < 0)
+			Duck.style.backgroundImage = 'url(../img/duck-r.gif)'
+		else
+			Duck.style.backgroundImage = 'url(../img/duck-l.gif)'
+
+		Duck.style.width = this.width + 'px';
+		Duck.style.height = this.height + 'px';
+		Duck.style.left = this.left + 'px';
+		Duck.style.top = this.top + 'px';
+
+		document.body.append(Duck);
+		return Duck;
+	}
+	Fly(Duck = this.initDuck()) {
+		let DuckFlyAnimation = setInterval(() => {
+			Duck.animate(
+										[
+											{ top: (Math.random() * screen.availHeight * 0.6 + screen.availHeight * 0.1) + 'px'},
+											{ top: (Math.random() * screen.availHeight * 0.6 + screen.availHeight * 0.1) + 'px'}
+										], 
+										{
+											easing: 'ease',
+											iterations: 1
+										}
+									)
+
+			if (this.direction < 0) {
+				this.left -= this.speed;
+				Duck.style.left = this.left + 'px';
+				if (this.left < -116) {
+					Duck.remove();
+					this.Out()
+					newGame.minusLife();
+				}
+			}
+			else {
+				this.left += this.speed
+				Duck.style.left = this.left + 'px';
+				if (this.left > (screen.availWidth + 16)) {
+					Duck.remove();
+					this.Out()
+					newGame.minusLife();
+				}
+			}
+		}, 4)
+
+		this.FlyAnimation = DuckFlyAnimation;
+	}
+
+	Died() {
+		clearInterval(this.FlyAnimation);
+		newGame.plusScore();
+	}
+	Out() {
+		clearInterval(this.FlyAnimation)
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+class Game {
+	constructor(bestScore = localStorage.getItem("bestScore"), lifeCount = 2) {
+		this.bestScore = (bestScore != null || bestScore != undefined) ? bestScore : 0,
+		this.score = 0,
+		this.life = lifeCount,
+		this.gameTimer,
+		this.AllDucks = [],
+		this.start()
+	}
+	start() {
+		if (localStorage.getItem('bestScore') != null || localStorage.getItem('bestScore') != undefined)
+			document.getElementById('best').innerHTML = localStorage.getItem('bestScore');
+		else
+			document.getElementById('best').innerHTML = 0;
+
+		let timer = setInterval(() => {
+			this.AllDucks.push(new Duck());
+		}, 1000)
+		this.gameTimer = timer;
+		for (let i = 0; i < document.getElementsByClassName('life').length; i++)
+			document.getElementsByClassName('life')[i].style.background = "url('../img/life++.svg')";
+			quack();
+	}
+	gameOver() {
+		if (this.bestScore < this.score)
+			this.bestScore = this.score;
+		this.score = 0;
+		localStorage.setItem("bestScore", this.bestScore)
+
+		document.getElementById('best').innerHTML = this.bestScore;
+		document.getElementById('score').innerHTML = this.score;
+		clearInterval(this.gameTimer);
+		this.AllDucks.forEach(Duck => {
+			Duck.Out();
+		})
+		document.body.querySelectorAll('.duck').forEach(duck => {
+			duck.remove();
+		})
+		play.style.display = 'block';
+		stats.style.display = 'none';
+
+		quack();
+	}
+	minusLife() {
+		document.getElementsByClassName('life')[this.life].style.background = "url('../img/life--.svg')"
+		if (this.life == 0)
+			this.gameOver()
+		else
+			this.life--;
+	}
+	plusScore() {
+		this.score++
+		document.getElementById('score').innerHTML = this.score;
+	}
+}
+// Adding ducks quacking while playing
+var ducks = document.createElement("audio");
+ducks.src = "../sound/ducks.ogg";
+ducks.volume = 0.0045;
+ducks.loop = true;
+function quack() {
+	if (ducks.paused)
+		ducks.play();
+	else
+		ducks.pause();
+}
+
+// Play button
+let play = document.getElementById('play'), stats = document.getElementById('wrapper');
+play.onclick = () => {
+	newGame = new Game();
+
+	play.style.display = 'none';
+	stats.style.display = 'flex';
+}
